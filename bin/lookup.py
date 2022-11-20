9efaf3b3d877c35f7d1ad81fc66cdc207a87e52e
peymanhr <phooshmand@gmail.com>
2022-11-21 02:01:18 +0330

    Add bin/lookup.py script

---
+++ bin/lookup.py
@@ -0,0 +1,11 @@
+from ipaddress import ip_network
+from sys import argv
+
+prefix_file = argv[1]
+ip_address = argv[2]
+
+with open (prefix_file) as f:
+    for prefix in f:
+        network = ip_network(prefix.strip())
+        if ip_address in [str(x) for x in network]:
+            print(network)
