9fbbe8e65a2cf31038e3056aa7e6bb5e2b0769a1
peymanhr <phooshmand@gmail.com>
2022-11-21 02:09:46 +0330

    Fix processing empty line at the end of the prefixes file

--- bin/lookup.py
+++ bin/lookup.py
@@ -6,6 +6,8 @@ ip_address = argv[2]
 
 with open (prefix_file) as f:
     for prefix in f:
-        network = ip_network(prefix.strip())
-        if ip_address in [str(x) for x in network]:
-            print(network)
+        prefix = prefix.strip()
+        if prefix:
+            network = ip_network(prefix.strip())
+            if ip_address in [str(x) for x in network]:
+                print(network)
