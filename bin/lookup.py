82d6c28c124082de2e4c68063d0d72c26c4b93fe
peymanhr <phooshmand@gmail.com>
2022-11-21 05:33:01 +0330

    Use faster method for lookup

--- bin/lookup.py
+++ bin/lookup.py
@@ -1,13 +1,13 @@
-from ipaddress import ip_network
+from ipaddress import ip_address, ip_network
 from sys import argv
 
 prefix_file = argv[1]
-ip_address = argv[2]
+ip = argv[2]
 
 with open (prefix_file) as f:
     for prefix in f:
         prefix = prefix.strip()
         if prefix:
             network = ip_network(prefix.strip())
-            if ip_address in [str(x) for x in network]:
+            if ip_address(ip) in network:
                 print(network)
